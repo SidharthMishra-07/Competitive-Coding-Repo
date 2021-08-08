113994d2cda0781616bd2ceb2fd1658bc883b077
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-08 23:32:06 +0530

    Files added

--- C++ Templates/OverloadingTemplate.cpp
+++ C++ Templates/OverloadingTemplate.cpp
@@ -2,13 +2,13 @@
 using namespace std;
  
 void func(int a){
-    cout<<"I am first func() "<<a<<endl;
+    cout<<"I am first func "<<a<<endl;
 }
  
 template<class T>
 void func(T a)
 {
-    cout<<"I am templatised func() "<<a<<endl;
+    cout<<"I am templatised func "<<a<<endl;
 }
 
 int main()
