5405b2f736608517470228b19d10c6185d86de8c
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-11 20:36:25 +0530

    Map updated

--- C++ STL/MapSTL.cpp
+++ C++ STL/MapSTL.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
+#include<map>
+#include<bits/stdc++.h>
 using namespace std;
+
+
 int main()
 {
+    map<string,int> marksMap;
+    marksMap["Boy1"]=100;
+    marksMap["Boy2"]=60;
+    marksMap["Boy3"]=70;
+    marksMap["Boy4"]=90;
+
+    map<string,int> :: iterator iter;
+
+    for(iter=marksMap.begin(); iter!= marksMap.end(); iter++)
+    {
+        cout<<(*iter).first<<" : "<<(*iter).second<<endl;
+    }
+
+
  return 0;
 }
