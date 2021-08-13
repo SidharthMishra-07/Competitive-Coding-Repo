2cd5eabc192dfa9fc0b7fa4be4dc8cc96f9678cd
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-13 22:23:45 +0530

    Files added

--- C++ STL/FunctionObject.cpp
+++ C++ STL/FunctionObject.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main()
 {
     // Function Objects (Functor) : A function wrapped in a class so that it is available like an object
-    int arr[] = {1, 73, 4, 25, 54, 37};
-    sort(arr,arr+6, greater<int>());
+    int arr[] = {1, 73, 4, 25, 54, 37, 89, 54};
+    sort(arr,arr+8, greater<int>());
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 8; i++)
     {
         cout<<arr[i]<<endl;
     }
