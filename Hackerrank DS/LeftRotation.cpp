98cd66547992435b823d9d8007633b1a2263222f
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-14 19:53:05 +0530

    FIles Added

--- Hackerrank DS/LeftRotation.cpp
+++ Hackerrank DS/LeftRotation.cpp
@@ -8,20 +8,20 @@ void rotateLeft(int d, vector<int> &arr) {
         arr.push_back(arr[0]);      //pushes the first element to the back of the array
         arr.erase(arr.begin());     //delete the first element..
     }
+    //Printing the Left Rotated array
+    
     for(int i=0;i<arr.size();i++)
     {
-        cout<<arr[i]<<endl;
+        cout<<arr[i]<<" ";
     }
 }
 
 int main()
 {
-    vector<int> vec = { 1, 2, 3, 4, 5, 6 };
-    int n = vec.size();
+    vector<int> vec = { 1, 2, 3, 4, 5 };
     int d = 4;
  
-    // Function call
-    rotateLeft(d % n , vec);
+    rotateLeft(d , vec);
  
     return 0;
 }
