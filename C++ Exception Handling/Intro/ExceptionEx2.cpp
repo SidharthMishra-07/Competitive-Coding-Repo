c9da763a33d0b78488a13b535dc40074f4b1004f
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-20 16:26:13 +0530

    Exception HAndling Added

---
+++ C++ Exception Handling/Intro/ExceptionEx2.cpp
@@ -0,0 +1,20 @@
+// 2) There is a special catch block called ‘catch all’ catch(…) that can be used to catch all types of exceptions. For example, in the following program, an int is thrown as an exception, but there is no catch block for int, so catch(…) block will be executed. 
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    try  {
+       throw 10;
+    }
+    catch (char *excp)  {
+        cout << "Caught " << excp;
+    }
+    catch (...)  {
+        cout << "Default Exception\n";
+    }
+    return 0;
+}
+
+// Here 10 is an int hence it doesnt go into the first catch().
